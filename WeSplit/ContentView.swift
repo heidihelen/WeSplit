76e8c908983f102f7d5446994ca89b82ebee4a3e
Heidi Helen Pilypas <42740566+heidihelen@users.noreply.github.com>
2021-04-04 20:46:29 +0930

    Removed multiple rows in the Form and added a NavigationView.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,21 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Form {
-            Section {
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
-            }
-            Section {
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    Text("Hello, world!")
+                }
             }
         }
     }
