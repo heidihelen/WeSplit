d59da72e56093a1ba3561445ad27f337d9706b1e
Heidi Helen Pilypas <42740566+heidihelen@users.noreply.github.com>
2021-04-05 20:20:30 +0930

    Added a navigation bar and title.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -13,19 +13,22 @@ struct ContentView: View {
     @State private var tipPercentage = 2
     let tipPercentages = [10, 15, 20, 25, 0]
     var body: some View {
-        Form {
-            Section {
-                TextField("Amount", text: $checkAmount)
-                    .keyboardType(.decimalPad)
-                Picker("Number of people", selection: $numberOfPeople) {
-                    ForEach(2 ..< 100) {
-                        Text("\($0) people")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
                     }
                 }
+                Section {
+                    Text("$\(checkAmount)")
+                }
             }
-            Section {
-                Text("$\(checkAmount)")
-            }
+            .navigationBarTitle("WeSplit")
         }
     }
 }
