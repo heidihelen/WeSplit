c0382f7944de7da1d71d533e19cd7f3e8d742230
Heidi Helen Pilypas <42740566+heidihelen@users.noreply.github.com>
2021-04-05 20:38:52 +0930

    Made the picker into a segmented control using the .pickerStyle(SegmentedPickerStyle()) modifier.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -30,6 +30,7 @@ struct ContentView: View {
                             Text("\(self.tipPercentages[$0])%")
                         }
                     }
+                    .pickerStyle(SegmentedPickerStyle())
                 }
                 Section {
                     Text("$\(checkAmount)")
