01d60cfe01907e8ee476eac56100ce9f2aec2688
Heidi Helen Pilypas <42740566+heidihelen@users.noreply.github.com>
2021-04-11 20:46:52 +0930

    Changed numberOfPeople to be an empty string from an Int because it is now entered using a TextField rather than a picker. Changed peopleCount from a constant to a variable so it can be mutated. Added a condition so if a user enters 0 people, it is changed to 1 to fix an error. Added a section header to the order amount TextField. Changed numberOfPeople from a picker to a TextField with 1 as the placeholder text.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,11 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = ""
-    @State private var numberOfPeople = 2
+    @State private var numberOfPeople = ""
     @State private var tipPercentage = 2
     let tipPercentages = [10, 15, 20, 25, 0]
     var totalPerPerson: Double {
-        let peopleCount = Double(numberOfPeople + 2)
+        var peopleCount = Double(numberOfPeople) ?? 1
+        // Condition added so if user enters 0 people it will add on 1 to deal with an error
+        if peopleCount == 0 {
+            peopleCount += 1
+        }
         let tipSelection = Double(tipPercentages[tipPercentage])
         let orderAmount = Double(checkAmount) ?? 0
         let tipValue = orderAmount / 100 * tipSelection
@@ -31,14 +35,13 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             Form {
-                Section {
-                    TextField("Amount", text: $checkAmount)
+                Section(header: Text("Enter order amount")) {
+                    TextField("0.00", text: $checkAmount)
                         .keyboardType(.decimalPad)
-                    Picker("Number of people", selection: $numberOfPeople) {
-                        ForEach(2 ..< 100) {
-                            Text("\($0) people")
-                        }
-                    }
+                }
+                Section(header: Text("Enter amount of people")) {
+                    TextField("1", text: $numberOfPeople)
+                        .keyboardType(.decimalPad)
                 }
                 Section(header: Text("How much tip do you want to leave?")) {
                     Picker("Tip percentage", selection: $tipPercentage) {
